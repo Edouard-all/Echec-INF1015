3fcf65a33a3be2aeddc85b21573c152e295b0ee4
Edouard-all <eefj.allaire@gmail.com>
2026-04-17 14:40:10 -0400

    EA: continue Dame modele

--- Chess/modeleDame.cpp
+++ Chess/modeleDame.cpp
@@ -8,5 +8,20 @@ modele::Dame::Dame(QObject *parent, pair<uint8_t,uint8_t> position)
 
 void modele::Dame::mettreAJourPositionsValides() {
     reinitialiserPositionsValides();
-    for (uint8_t)
+    for (uint8_t i = 0; i < 8; i++) {
+        if (i != position_.second)
+            positionsValides_.push_back(make_pair(position_.first,i));
+        if (i != position_.first)
+            positionsValides_.push_back(make_pair(i,position_.second));
+        for(uint8_t j = 0; i < 8; j++) {
+            if (j + i == i + position_.first + position_.second)
+                continue;
+        }
+    }
+    for (uint8_t i = 0; i < 8; i++) {
+        if (i != position_.first) {
+            positionsValides_.push_back(make_pair(i,position_.second));
+        }
+    }
+
 }
