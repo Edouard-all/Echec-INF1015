ed7ac88c6884aba6630f3f8c877a0998210fc3e2
Aliou Diallo <alioudiallo@MacBook-Pro-de-Abdoulaye-281.local>
2026-05-04 10:58:49 -0400

    Creation du fichier Jeu

---
+++ Chess/modeleJeu.cpp
@@ -0,0 +1,5 @@
+#include "modeleJeu.h"
+
+modeleJeu::modeleJeu(QObject *parent)
+    : QObject{parent}
+{}
---
+++ Chess/modeleJeu.h
@@ -0,0 +1,15 @@
+#ifndef MODELEJEU_H
+#define MODELEJEU_H
+
+#include <QObject>
+
+class modeleJeu : public QObject
+{
+    Q_OBJECT
+public:
+    explicit modeleJeu(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // MODELEJEU_H
