ff69ab7f282c7687dc7d12833e3e6beb9276d6e6
Aliou Diallo <alioudiallo@MacBook-Pro-de-Abdoulaye-281.local>
2026-05-05 10:09:24 -0400

    La piece s affiche

--- Chess/vuePiece.cpp
+++ Chess/vuePiece.cpp
@@ -4,7 +4,7 @@ namespace vue {
 Piece::Piece(QWidget *parent)
     : QWidget{parent}
 {
-    piece_ = new QLabel(this);
+    piece_ = new QLabel();
 }
 
 Piece::~Piece() {
@@ -16,7 +16,7 @@ void Piece::setCouleur(bool estNoir) {
 }
 
 void Piece::dessinerPiece(QGraphicsScene* scene) {
-    QPixmap image("C:/Users/Édouard/Documents/Polytechnique/H2026/INF1015/Echec-INF1015/Chess/Images/LightKing.jpg");
+    QPixmap image(":/images/Images/LightKing.jpg");
     QPixmap imageRedimentionne = image.scaled(echiquier_.getTailleCarre(), echiquier_.getTailleCarre());
     piece_->setPixmap(imageRedimentionne);
     QGraphicsProxyWidget *proxy = scene->addWidget(piece_);
--- Chess/vuePiece.h
+++ Chess/vuePiece.h
@@ -25,7 +25,7 @@ signals:
 private:
     bool estNoir_;
     pair<int,int> position_;
-    QLabel *piece_;
+    QLabel* piece_;
     Echiquier echiquier_;
     //QPixmap image_(":/Images/DarkKing.jpg");
 };
